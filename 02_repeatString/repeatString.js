3bd78fc2f9313d0f3d5b6721b7d756476a9e74fe
dstein <dstein555@gmail.com>
2022-03-17 06:33:34 -0500

    Passed  all test cases for the 02_repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(repeatWord, timesToRepeat) {
+
+    let finalString = '';
+
+    if ( timesToRepeat >= 0 ) {
+
+        for ( let i = 1; i <= timesToRepeat; i++ ) {
+            finalString += repeatWord;
+        }
+
+    } else {
+        finalString = 'ERROR';
+    }
+
+    return finalString;
 
 };
 
