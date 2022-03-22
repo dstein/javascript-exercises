44bf26e8054847fbc326c9136916cf012eef7f2d
dstein <dstein555@gmail.com>
2022-03-22 06:23:51 -0500

    Finished code to pass all tests. Utilizes .filter array method.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function( myArray, toRemove ) {
 
+    const removeArgArray = Array.from( arguments );
+    removeArgArray.splice(0, 1); //remove first arg
+
+    const modArray = myArray.filter( (item, index) => {
+
+        if ( ! removeArgArray.includes(item) ) {
+            return true;
+        }
+
+    });
+
+    //Alt:
+    //const modArray = myArray.filter( (item) => ! removeArgArray.includes(item) );
+    //Array.indexOf can also be used by checking for values in myArray not included
+    //in the removeArgArray
+
+
+    return modArray;
 };
 
 // Do not edit below this line
