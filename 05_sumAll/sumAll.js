3b032c34e89123dfe3a577fbd9d198f53496aa04
dstein <dstein555@gmail.com>
2022-03-23 06:13:57 -0500

    Finished the sumAll exercise and passed all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,42 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+
+    if ( startInt < 0 || endInt < 0 ) {
+        return "ERROR";
+    }
+
+    if ( ! Number.isInteger(startInt) || ! Number.isInteger(endInt) ) {
+        return "ERROR";
+    }
+
+    const range = [];
+    let endSum = 0;
+
+    if ( startInt < endInt ) {
+
+        for ( let i = startInt; i <= endInt; i++ ) {
+            range.push(i);
+        }
+
+    } else {
+
+        for ( let i = endInt; i <= startInt; i++ ) {
+            range.push(i);
+        }
+
+    }
+
+
+    if ( range.length ) {
+
+        for ( rangeItem of range ) {
+            endSum += rangeItem;
+        }
+
+    } else {
+        //Error
+    }
+
+    return endSum;
 
 };
 
