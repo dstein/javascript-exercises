2cd93a8c2a97450f04880942077d613355cbad5a
dstein <dstein555@gmail.com>
2022-03-23 06:42:16 -0500

    Finished the leapYears exercise and passed all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    if ( year % 4 == 0 && year % 100 != 0 ) {
+        isLeapYear = true;
+    } else if ( year % 4 == 0 && year % 100 == 0 && year % 400 == 0 ) {
+        isLeapYear = true;
+    } else {
+        //Error
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
