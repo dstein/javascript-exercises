64a6f37d813f404565dbf865985860e15e3b6a6a
dstein <dstein555@gmail.com>
2022-05-03 06:09:48 -0500

    Finished temperature conversion logic and passed tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let tempCelsius = (temp - 32) / 1.8;
+  let parseTemp = tempCelsius.toFixed(1);
 
+  return parseFloat(parseTemp);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let tempFar = (temp * 1.8) + 32;
+  let parseTemp = tempFar.toFixed(1);
 
+  return parseFloat(parseTemp);
 };
 
 // Do not edit below this line
