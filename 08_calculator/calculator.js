98aeadf8511baf01cfa31c346ad565a241c0eb41
dstein <dstein555@gmail.com>
2022-08-23 06:22:35 -0500

    Finished the calculator testing.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let total = 0;
+
+  for ( const numItem in numArray ) {
+    total += parseInt( numArray[numItem] );
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let total = 1;
 
+  for ( const numItem in numArray ) {
+    total *= parseInt( numArray[numItem] );
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, powNum) {
+	return Math.pow(num1, powNum);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+
+  for ( let i = 2; i <= num; i++ ) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
