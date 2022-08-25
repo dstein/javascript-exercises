b6ea6b8f218a656ded29de2e3eebd90aba7cdde8
dstein <dstein555@gmail.com>
2022-08-25 06:11:02 -0500

    Finished palindromes exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (paliString) {
+    const paliArray = paliString.split('');
+    let finalPali   = "";
+    let startPali   = "";
 
+    const paliFilter = paliArray.filter( (item) => {
+        // !! converts result to bool
+        let result = !!item.match( /^[.,:!? ]/ );
+
+        if ( ! result ) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    startPali = paliFilter.join('').toLowerCase();
+    finalPali = paliFilter.reverse('').join('').toLowerCase();
+
+    if ( startPali === finalPali ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
